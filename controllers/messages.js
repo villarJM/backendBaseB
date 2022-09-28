02fd6275b371fbbf330c43dc72fa4aa628bf1642
MISAEL VILLAR JULIAN <l19350325@tuxtepec.tecnm.mx>
2022-09-28 08:41:32 -0500

    Implementacion del HTTP status Code

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,27 +1,27 @@
 const { request, response } = require("express")
 
 const rootMessage = (req = request, res = response) => {
-    res.json({msg:'Mensajes'})
+    res.status(404).json({msg:'Mensajes'})
 }
 
 const hiMessage = (req, res) => {
-    res.json({msg:'Hola mundo'})
+    res.status(408).json({msg:'Hola mundo'})
 }
 
 const byeMessage = (req, res) => {
-    res.json({msg:'Adiós mundo'})
+    res.status(205).json({msg:'Adiós mundo'})
 }
 
 const postMessage = (req, res) => {
-    res.json({msg:'Mensaje POST'})
+    res.status(429).json({msg:'Mensaje POST'})
 }
 
 const putMessage = (req, res) => {
-    res.json({msg:'Mensaje PUT'})
+    res.status(203).json({msg:'Mensaje PUT'})
 }
 
 const deleteMessage = (req, res) => {
-    res.json({msg:'Mensaje DELETE'})
+    res.status(401).json({msg:'Mensaje DELETE'})
 }
 
 module.exports = {rootMessage, hiMessage, byeMessage, postMessage, putMessage, deleteMessage}
